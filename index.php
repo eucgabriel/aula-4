a0c9894b98acbc328a7d65b29809329972faa97f
Carlos Gabriel <canalcarlosgabriel1407@gmail.com>
2022-02-01 17:08:54 -0300

    iniciando em php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CG</title>
+</head>
+<body>
+    <?php
+        $idade = 3;
+        $sal = 1825.54;
+        $nome = "Carlos Gabriel";
+        $casado = false;
+        echo $idade;
+        echo $sal;
+        echo $nome;
+        echo $casado;
+    ?>
+</body>
+</html>
